d2dee6826e39eb99d7325d341bbc9d690f89470e
BrittanyThirty <bmkthirty@gmail.com>
2018-01-28 17:07:52 -0800

    minor adjustments to html, update logic js, jq

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,10 @@
 
  function Pizza(size){}
    this.size = size;
-   this.toppings = []; 
+   this.toppings = [];
  };
 
-Pizza.prototype = function(){
+Pizza.prototype = function() {
   var cost = 15;
 }
   if(this.size === "Personal") {
@@ -15,14 +15,18 @@ Pizza.prototype = function(){
     cost *= 2.5;
   }
 
-
-
-
+if(this.toppings === )
 
 
 
 $(function(){
 $("form#order-form").submit(function(event){
   event.preventDefault();
+
+
+  var inputtedPizzaSize = $("select#pizza-size").val();
+  var newPizza = new Pizza (inputtedPizzaSize);
+
+
 }
 })
