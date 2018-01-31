534a4ee34223bbfd5ea05a885d33ede3b0862804
BrittanyThirty <bmkthirty@gmail.com>
2018-01-30 17:05:01 -0800

    add working logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,48 +1,48 @@
 //Business
-function Pizza(size,toppings) {
+function Pizza(size,toppings) { //constructor function
   this.size = size;
   this.toppings = toppings;
-}
+};
+
+Pizza.prototype.price = function(){ //prototype property
   var price = 15;
-Pizza.prototype.price = function() {
-  if(this.size === "personal"){
+  if(this.size === "Personal") {
     price *= 1;
-  } else if (this.size === "medium"){
+  } else if (this.size === "Medium"){
     price *= 2;
   } else {
     price *= 2.5;
   }
-    if(this.toppings >= 3) {
-      price *= 1.07;
-    } else if (this.toppings === 2) {
-      price *= 1.05;
-    } else {
-      price *= 1;
-    };
-      return Pizza.price ();
-};
 
+  if(this.toppings.length >= 3) {
+    price *= 1.07;
+  } else if (this.toppings.length === 2) {
+    price *= 1.05;
+  } else {
+    price *= 1;
+  }
+  return price; //return price does not call the function
+}               //must name and call function as shown
+                // below in UI logic w var result = yourPizza.price();
 
 // //UI
-$(document).ready(function(){
-  $('.submit#button_id').submit(function() {
-    alert("test");
-  // $("form#submit").click(function(){
+$(document).ready(function() {
+
+  $("form#order-form").submit(function() {
     event.preventDefault();
 
-});
-})
+    var pickSize = $("select#pizza-size").val();
+    var arrTop = [];
+    var yourPizza = new Pizza(pickSize, arrTop);
 
-var yourPizza = new Pizza(pickSize, pickToppings);
-var pickSize = $("pizza-size").val();
-var pickToppings = [];
-
-// console.log("it works!");
     $('input[name="toppings"]:checked').each(function() {
-      pickSize.push($(this.total));
+      arrTop.push($(this).val());
 
-      // $("#pick-size").append("<li>"+ this.total + "</li>"){
-//  $(".total-due").text(yourPizza)
-//  $(".pizza-size").text(new Pizza);
-// // });
+    var result = yourPizza.price(); //var somevariable = then call your var
+    yourPizza.price(); //calling proto price function
+      $("#total-due").show();
+      $("#total-due").text(result); //result from var result
+
     });
+  });
+});
